35727ae561fdbe7323edc06e9395986191b5d173
Danny <danny.belmonte4@gmail.com>
2025-09-19 14:56:30 -0500

    updated base route

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/stats-client/',
+  base: '/',
   plugins: [react({
     // Additional React plugin options if needed
     jsxRuntime: 'automatic',
